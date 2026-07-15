c2131f6c775c553d7064cf8578d31d7f76f96ac1
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2026-07-15 00:57:06 -0400

    Database configuration for Mysql and migrations

--- start/env.ts
+++ start/env.ts
@@ -18,6 +18,18 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
+  /*
+  |----------------------------------------------------------
+  | Variables de base de datos
+  |----------------------------------------------------------
+  */
+  DB_CONNECTION: Env.schema.string(),
+  DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_PORT: Env.schema.number(),
+  DB_USER: Env.schema.string(),
+  DB_PASSWORD: Env.schema.string.optional(),
+  DB_DATABASE: Env.schema.string(),
+
   // App
   APP_KEY: Env.schema.secret(),
   APP_URL: Env.schema.string({ format: 'url', tld: false }),
